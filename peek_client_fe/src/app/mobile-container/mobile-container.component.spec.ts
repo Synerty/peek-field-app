bb6c867f08916bb6d93be70bef4be292a15c58f5
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-07 21:02:15 +1000

    Deleted desktop container component

--- peek_client_fe/src/app/mobile-container/mobile-container.component.spec.ts
+++ peek_client_fe/src/app/mobile-container/mobile-container.component.spec.ts
@@ -1,28 +1,28 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
-import { MobileContainerComponent } from './mobile-container.component';
-
-describe('MobileContainerComponent', () => {
-  let component: MobileContainerComponent;
-  let fixture: ComponentFixture<MobileContainerComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MobileContainerComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MobileContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// /* tslint:disable:no-unused-variable */
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { By } from '@angular/platform-browser';
+// import { DebugElement } from '@angular/core';
+//
+// import { MobileContainerComponent } from './mobile-container.component';
+//
+// describe('MobileContainerComponent', () => {
+//   let component: MobileContainerComponent;
+//   let fixture: ComponentFixture<MobileContainerComponent>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ MobileContainerComponent ]
+//     })
+//     .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(MobileContainerComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
