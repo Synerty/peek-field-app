028479ea8f5097465424967616bd1817f15da8f8
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-08 19:24:25 +1000

    Introduced protractor back into project

--- peek_client_fe/src/app/home/home-icons/home-icons.component.spec.ts
+++ peek_client_fe/src/app/home/home-icons/home-icons.component.spec.ts
@@ -1,28 +1,28 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
-//
-// import { HomeIconsComponent } from './home-icons.component';
-//
-// describe('HomeIconsComponent', () => {
-//   let component: HomeIconsComponent;
-//   let fixture: ComponentFixture<HomeIconsComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ HomeIconsComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(HomeIconsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { HomeIconsComponent } from './home-icons.component';
+
+describe('HomeIconsComponent', () => {
+  let component: HomeIconsComponent;
+  let fixture: ComponentFixture<HomeIconsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeIconsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeIconsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- peek_client_fe/src/app/mobile-container/mobile-container.component.spec.ts
+++ peek_client_fe/src/app/mobile-container/mobile-container.component.spec.ts
@@ -1,28 +1,28 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
-//
-// import { MobileContainerComponent } from './mobile-container.component';
-//
-// describe('MobileContainerComponent', () => {
-//   let component: MobileContainerComponent;
-//   let fixture: ComponentFixture<MobileContainerComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ MobileContainerComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(MobileContainerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+/* tslint:disable:no-unused-variable */
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
+import {MobileContainerComponent} from "./mobile-container.component";
+import {VortexService, ComponentLifecycleEventEmitter} from "@synerty/vortexjs";
+import {ActivatedRoute} from "@angular/router";
+
+describe('MobileContainerComponent', () => {
+    let component: MobileContainerComponent;
+    let fixture: ComponentFixture<MobileContainerComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [MobileContainerComponent],
+            providers: [VortexService, ActivatedRoute]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MobileContainerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
