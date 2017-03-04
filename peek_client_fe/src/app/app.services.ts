7238bcd2555ef0d1b05f1cad5c78b29ae87dd9d1
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-04 18:09:30 +1000

    Added support for badge counts in title bar links

--- peek_client_fe/src/app/app.services.ts
+++ peek_client_fe/src/app/app.services.ts
@@ -1,10 +1,18 @@
 import {Ng2BalloonMsgService} from "@synerty/ng2-balloon-msg";
 import {VortexService, VortexStatusService} from "@synerty/vortexjs";
 import {TitleService} from "@synerty/peek-client-fe-util";
+import {titleBarLinks} from "./plugin-title-bar-links";
 
 
-export let peekRootServices = [TitleService, Ng2BalloonMsgService,
+export let peekRootServices = [
+    {
+        provide: TitleService,
+        useValue: new TitleService(titleBarLinks)
+    },
+    Ng2BalloonMsgService,
+
     // Vortex Services
-    VortexStatusService, VortexService
+    VortexStatusService,
+    VortexService
 ];
 
--- peek_client_fe/src/app/main-title/main-title.component.ts
+++ peek_client_fe/src/app/main-title/main-title.component.ts
@@ -1,9 +1,8 @@
 import {OnInit, OnDestroy} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 import {PeekComponent} from "@synerty/peek-web-ns";
-import {TitleService} from "@synerty/peek-client-fe-util";
+import {TitleService, TitleBarLink} from "@synerty/peek-client-fe-util";
 import {VortexStatusService} from "@synerty/vortexjs";
-import {titleBarLinks} from "../plugin-title-bar-links";
 
 @PeekComponent({
     selector: "peek-main-title",
@@ -22,29 +21,45 @@ export class MainTitleComponent implements OnInit, OnDestroy {
     vortexIsOnline:boolean= false;
 
     constructor(vortexStatusService:VortexStatusService, titleService: TitleService) {
+        this.leftLinks = titleService.leftLinksSnapshot;
+        this.rightLinks = titleService.rightLinksSnapshot;
+
         this.subscriptions.push(
             titleService.title.subscribe(title => this.title = title));
 
         this.subscriptions.push(
+            titleService.leftLinks.subscribe(links => this.leftLinks = links));
+
+        this.subscriptions.push(
+            titleService.rightLinks.subscribe(links => this.rightLinks = links));
+
+        this.subscriptions.push(
             vortexStatusService.isOnline.subscribe(v => this.vortexIsOnline = v));
 
-        for (let link of titleBarLinks) {
-            if (link.left)
-                this.leftLinks.push(link);
-            else
-                this.rightLinks.push(link);
-        }
     }
 
     ngOnInit() {
-        // this.routeSubscription = this.route
-        //     .data
-        //     .subscribe(v => this.title = "xx" + JSON.stringify(v));
     }
 
     ngOnDestroy() {
         while (this.subscriptions.length > 0)
             this.subscriptions.pop().unsubscribe();
+    }
+
+    // ------------------------------
+    // Display methods
+
+    linkTitle(title:TitleBarLink) {
+        if (title.badgeCount == null) {
+            return title.text;
+        }
+
+        if (title.left) {
+            return `${title.text} (${title.badgeCount})`;
+        }
+
+        return `(${title.badgeCount}) ${title.text}`;
+
     }
 }
 
