af1c2b632efbfcd26baa6b30b2f445a10492e94c
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-05-25 13:34:50 +1200

    resolved merge conflict

--- peek_mobile/src/app/main-home/main-home.component.ts
+++ peek_mobile/src/app/main-home/main-home.component.ts
@@ -1,40 +1,37 @@
 import {Component, OnInit} from "@angular/core";
 import {
-  ComponentLifecycleEventEmitter,
-  Tuple,
-  TupleDataOfflineObserverService,
-  TupleSelector
+    ComponentLifecycleEventEmitter,
+    TupleDataOfflineObserverService
 } from "@synerty/vortexjs";
 import {TitleService} from "@synerty/peek-mobile-util";
 import {homeLinks} from "../../plugin-home-links";
 
 
 @Component({
-  selector: "peek-main-home",
-  templateUrl: 'main-home.component.web.html',
-  moduleId: module.id
+    selector: "peek-main-home",
+    templateUrl: 'main-home.component.web.html',
+    moduleId: module.id
 })
 export class MainHomeComponent extends ComponentLifecycleEventEmitter implements OnInit {
 
-  appDetails = homeLinks;
-  myParseInt = parseInt;
+    appDetails = homeLinks;
 
-  constructor(tupleDataObserver: TupleDataOfflineObserverService, titleService: TitleService) {
-    super();
-    titleService.setTitle("Peek Home");
+    constructor(tupleDataObserver: TupleDataOfflineObserverService, titleService: TitleService) {
+        super();
+        titleService.setTitle("Peek Home");
 
-  }
-
-  ngOnInit() {
-  }
-
-  appButtonGridRows(): string {
-    let val = "auto";
-    let rowCount = parseInt((this.appDetails.length / 2).toString());
-    for (let i = 0; i < rowCount; i++) {
-      val += ", auto";
     }
-    return val;
-  }
+
+    ngOnInit() {
+    }
+
+    appButtonGridRows(): string {
+        let val = "auto";
+        for (let i = 0; i < this.appDetails.length / 2; i++) {
+            val += ", auto";
+        }
+        return val;
+    }
 
 }
+
