359ba7f3845b8594bfe2461251e65f84e756f7e0
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-06-30 20:16:31 +1000

    Updated package dependencies

--- peek_mobile/build-ns/app/main.ts
+++ peek_mobile/build-ns/app/main.ts
@@ -20,29 +20,5 @@ import "@synerty/vortexjs";
 // }
 // This should be last
 import {AppNsModule} from "./app.ns.module";
-// ---------- BEGIN PLUGIN CONTENT UPDATE
-import * as application from "application";
-import {SyncStatus} from "nativescript-peek-update/code-push-lib";
-import {CodePush} from "nativescript-peek-update/code-push";
-import {isIOS} from "platform";
-
-// add this in some central place that's executed once in a lifecycle
-application.on(application.resumeEvent, () => {
-
-    let codePush = new CodePush();
-
-    codePush.sync({
-        deploymentKey: isIOS ? "your-ios-deployment-key" : "your-android-deployment-key"
-    })
-        .then((syncStatus: SyncStatus): void => {
-            console.log("CodePush syncStatus: " + syncStatus);
-            if (syncStatus === SyncStatus.UP_TO_DATE) {
-                console.log("CodePush: no pending updates; you're running the latest version!");
-            } else if (syncStatus === SyncStatus.UPDATE_INSTALLED) {
-                console.log("CodePush: update installed - it will be activated upon next cold boot");
-            }
-        });
-});
-// ---------- END PLUGIN CONTENT UPDATE
 
 platformNativeScriptDynamic().bootstrapModule(AppNsModule);
