62eb14181e792c69a242791c7f2ddabdb5871ad1
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-07-27 17:10:11 +1000

    Vortex 0.8.8 compatibility fixes

--- peek_mobile/build-ns/app/main.ts
+++ peek_mobile/build-ns/app/main.ts
@@ -2,14 +2,12 @@
 import {platformNativeScriptDynamic} from "nativescript-angular/platform";
 
 import "nativescript-websockets";
-import "nativescript-localstorage";
 import "rxjs/add/operator/filter";
 import "moment";
-// Set the URL for the vortex
-import {VortexService} from "@synerty/vortexjs";
-// VortexService.setVortexUrl(`http://${host}:8000/vortex`);
+
 // Import some stuff that we need
 import "@synerty/vortexjs";
+
 // import "nativescript-angular";
 // // Potentially enable angular prod mode
 // import {enableProdMode} from "@angular/core";
