67e25367826f008993e935762b5d0e5ebb534d0e
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-07-02 22:33:41 +1000

    Upgraded to angular6, backing out ~@peek change
    ORMOB-644

--- peek_mobile/build-web/src/typings.d.ts
+++ peek_mobile/build-web/src/typings.d.ts
@@ -1,9 +1,9 @@
 /* SystemJS module definition */
-// declare let module: {
-//   id: string;
-// };
-//
-// declare let require: any;
+declare let module: {
+  id: string;
+};
+
+declare let require: any;
 
 declare module "worker-loader!*" {
     const content: any;
