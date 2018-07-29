c3b32ad4bc80de331814e575e682364c2717fc6a
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-07-29 20:06:48 +1000

    Diagram now offline caches lookups so it can load offline.
    ORMOB-661

--- peek_mobile/build-ns/webpack.config.js
+++ peek_mobile/build-ns/webpack.config.js
@@ -211,7 +211,7 @@ module.exports = env => {
                 { from: "fonts/**" },
                 { from: "**/*.jpg" },
                 { from: "**/*.png" },
-                { from: "**/*.mp3" },
+                { from: "assets/**"},
             ], { ignore: [`${relative(appPath, appResourcesFullPath)}/**`] }),
             // Generate a bundle starter script and activate it in package.json
             new nsWebpack.GenerateBundleStarterPlugin([
