1bc12239c1a25368b9a823dc30daa5145a29c704
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-08-09 14:15:57 +1000

    packaging frontend apps now includes webpack.config.js, etc
    ORMOB-678

--- peek_mobile/rsync_to_mac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rsync -avP build-ns /media/psf/stash  --exclude platforms --exclude node_modules
-
-rsync -avP build-ns/hooks /media/psf/stash/build-ns/
-
-
--- peek_mobile/tar_build_ns.sh
+++
@@ -1,1 +0,0 @@
-tar cvjf build-ns.tar.bz2 --exclude node_modules --exclude platforms build-ns
