54662a63eccc77f20dbf0bec99e34166c6840628
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-19 14:53:27 +1000

    Updated package publish scripts.
    PEEK-321

---
+++ publish.settings.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PY_PACKAGE="peek_mobile"
+PYPI_PUBLISH="1"
+
+VER_FILES=""
+VER_FILES="${VER_FILES} peek_mobile/build-ns/package.json"
+VER_FILES="${VER_FILES} peek_mobile/build-ns/app/App_Resources/iOS/Info.plist"
+VER_FILES="${VER_FILES} peek_mobile/build-ns/app/App_Resources/Android/AndroidManifest.xml"
+VER_FILES="${VER_FILES} peek_mobile/src/app/main-config/main-config.component.ns.html"
+VER_FILES="${VER_FILES} peek_mobile/src/app/main-config/main-config.component.web.html"
