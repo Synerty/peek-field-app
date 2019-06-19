fc616b1ad7685902e5faa37e32a680b3fa476556
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-19 15:15:22 +1000

    publish script now uses twine to upload
    Added option to push to github
    
    PEEK-321

--- publish.settings.sh
+++ publish.settings.sh
@@ -3,7 +3,10 @@
 PY_PACKAGE="peek_mobile"
 PYPI_PUBLISH="1"
 
+GITHUB_PUSH="1"
+
 VER_FILES=""
+
 VER_FILES="${VER_FILES} peek_mobile/build-ns/package.json"
 VER_FILES="${VER_FILES} peek_mobile/build-ns/app/App_Resources/iOS/Info.plist"
 VER_FILES="${VER_FILES} peek_mobile/build-ns/app/App_Resources/Android/AndroidManifest.xml"
