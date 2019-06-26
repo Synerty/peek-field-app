5cee87aa3df1f15add74c8c22163e205e1d8078b
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-26 22:51:18 +1200

    Added support for comitting SOME version files in publish.sh
    PEEK-321

--- publish.settings.sh
+++ publish.settings.sh
@@ -5,8 +5,9 @@ PYPI_PUBLISH="1"
 
 GITHUB_PUSH="1"
 
-VER_FILES=""
+VER_FILES_TO_COMMIT=""
 
+VER_FILES=""
 VER_FILES="${VER_FILES} peek_mobile/build-ns/package.json"
 VER_FILES="${VER_FILES} peek_mobile/build-ns/app/App_Resources/iOS/Info.plist"
 VER_FILES="${VER_FILES} peek_mobile/build-ns/app/App_Resources/Android/AndroidManifest.xml"
