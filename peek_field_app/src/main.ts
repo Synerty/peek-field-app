0fddf720b6287efdfdd3485250a9c09a399ce870
Jarrod Chesney <jarrod.chesney@synerty.com>
2022-05-22 19:18:50 +1000

    PEEK-1534 Add worker support

--- peek_field_app/src/main.ts
+++ peek_field_app/src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 // Enable the use of workers for the payload
-import { VortexService } from "@synerty/vortexjs";
+import { Payload, PayloadDelegateWeb, VortexService } from "@synerty/vortexjs";
 // Potentially enable angular prod mode
 import { enableProdMode } from "@angular/core";
 import { environment } from "./environments/environment";
@@ -8,14 +8,12 @@ import { AppModule } from "./app/app.module";
 import { defineCustomElements } from "@ionic/pwa-elements/loader";
 
 const protocol = location.protocol.toLowerCase() === "https:" ? "wss" : "ws";
-
 VortexService.setVortexUrl(
     `${protocol}://${location.hostname}:${location.port}/vortexws`
 );
 VortexService.setVortexClientName("peek-field-app");
 
-// Payload.setWorkerDelegate(new PayloadDelegateWeb());
-
+Payload.setWorkerDelegate(new PayloadDelegateWeb());
 if (environment.production) {
     enableProdMode();
 }
