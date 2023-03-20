c1a4e470a7cb7b9ba17d09c634d6eb76aec2f8d9
Jarrod Chesney <jarrod.chesney@synerty.com>
2023-03-20 21:04:37 +1000

    PEEK-2055 Disabled shape to undo for capacitor app

--- peek_field_app/ios/App/App/AppDelegate.swift
+++ peek_field_app/ios/App/App/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    application.applicationSupportsShakeToEdit = false;
     // Override point for customization after application launch.
     return true
   }
